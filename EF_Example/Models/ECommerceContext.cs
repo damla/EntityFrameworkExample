6de81ad5b642ce64d051927a69fda6e1ac350103
Damla Koksal <damlakoksal@outlook.com>
2019-07-05 14:11:04 +0300

    Context added.

---
+++ EF_Example/Models/ECommerceContext.cs
@@ -0,0 +1,14 @@
+﻿using EF_Example.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EF_Example.Models
+{
+    public class ECommerceContext: DbContext
+    {
+        public DbSet<User> Users { get; set; }
+    }
+}
