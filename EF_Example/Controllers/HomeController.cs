fc735d500cd7c918dde5d615329a699d8d280bae
Damla Koksal <damlakoksal@outlook.com>
2019-07-05 13:40:24 +0300

    Controllers folder added.

---
+++ EF_Example/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EF_Example.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "This is Home controller's Index action!";
+        }
+    }
+}
